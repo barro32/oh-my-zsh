b4110c0e7c60476ecbb6122da8c2fdf44e676cec
Dan Barrington <daniel.barrington@pancentric.com>
2018-04-16 13:25:20 +0100

    Initial

---
+++ plugins/custom_vagrant/custom_vagrant.plugin.zsh
@@ -0,0 +1,3 @@
+alias vagup='(cd ~/vagrant-boxes/panbox-python && vagrant up; vagrant ssh)'
+alias vagdown='(cd ~/vagrant-boxes/panbox-python && vagrant halt)'
+alias vagssh='(cd ~/vagrant-boxes/panbox-python && vagrant ssh)'
