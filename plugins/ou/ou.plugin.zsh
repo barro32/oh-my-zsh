6dfed45275dfa0e44a19217d4a34583635b28951
Dan Barrington <daniel.barrington@pancentric.com>
2018-05-02 13:38:29 +0100

    Update custom plugins and themes

--- plugins/ou/ou.plugin.zsh
+++ plugins/ou/ou.plugin.zsh
@@ -1,8 +1,11 @@
-alias oubuild='(cd ~/dev/bfs/ou/ou-config/BritishFriendly; ant -f Build.xml -Dou.home=/home/daniel.barrington/dev/bfs/ou/system)'
-alias oupageflow='(cd ~/dev/bfs/ou/ou-config/BritishFriendly/Base && ant -f Build.xml -Dou.home=/home/daniel.barrington/dev/bfs/ou/system)'
-alias ouworkflow='(cd ~/dev/bfs/ou/ou-config/BritishFriendly; ant -f Build.xml -Dou.home=/home/daniel.barrington/dev/bfs/ou/system -Dworkflows.force-unload=true)'
-alias oudeploy='(cd ~/dev/bfs/ou/ou-config/BritishFriendly; ant -f Build.xml -Dou.home=/home/daniel.barrington/dev/bfs/ou/system -Dwebdav.host=52.210.69.137 -Dwebdav.port=80)'
-alias oustart='cd ~/dev/bfs/ou/system; ant -f Build.xml -DDdbusername=root -Ddbpassword=Pa55w0rd -DJBOSS_OPTS="-b 172.31.90.48" start'
+# BFS Product
+alias oubuildall='(cd /home/daniel.barrington/dev/bfs/ou/ou-config/BritishFriendly; ant -f Build.xml -Dwebdav.host=172.31.90.48 -Dwebdav.port=8080 -Dou.home=/home/daniel.barrington/dev/bfs/ou/system)'
+alias oubuild='(cd /home/daniel.barrington/dev/bfs/ou/ou-config/BritishFriendly/Base && ant -f Build.xml  -Dwebdav.host=172.31.90.48 -Dwebdav.port=8080 -Dou.home=/home/daniel.barrington/dev/bfs/ou/system)'
+alias ouworkflow='(cd /home/daniel.barrington/dev/bfs/ou/ou-config/BritishFriendly; ant -f Build.xml -Dou.home=/home/daniel.barrington/dev/bfs/ou/system -Dworkflows.force-unload=true)'
+alias oudeploy='(cd /home/daniel.barrington/dev/bfs/ou/ou-config/BritishFriendly; ant -f Build.xml -Dou.home=/home/daniel.barrington/dev/bfs/ou/system -Dwebdav.host=52.210.69.137 -Dwebdav.port=80)'
+alias build='ant -f Build.xml -Dwebdav.host=172.31.90.48 -Dwebdav.port=8080 -Dou.home=/home/daniel.barrington/dev/bfs/ou/system'
+# OU
+alias oustart='cd /home/daniel.barrington/dev/bfs/ou/system; ant -f Build.xml -DDdbusername=root -Ddbpassword=Pa55w0rd -Dmysql.root.password=Pa55w0rd -Ddbhost=127.0.0.1 -DJBOSS_OPTS="-b 172.31.90.48" start'
 alias oustop='ant -f Build.xml stop'
-alias ousetup='cd ~/dev/bfs/ou/system; ant -f Build.xml -Ddbusername=root -Ddbpassword=Pa55w0rd -Ddbhost=127.0.0.1 -DJBOSS_OPTS="-b 172.31.90.48" stop teardown setup start'
-alias outeardown='cd ~/dev/bfs/ou/system; ant -f Build.xml -Ddbusername=root -Ddbpassword=Pa55w0rd -Ddbhost=127.0.0.1 teardown'
+alias ousetup='cd /home/daniel.barrington/dev/bfs/ou/system; ant -f Build.xml -Ddbusername=root -Ddbpassword=Pa55w0rd -Dmysql.root.password=Pa55w0rd -Ddbhost=127.0.0.1 -DJBOSS_OPTS="-b 172.31.90.48" stop teardown setup'
+alias outeardown='cd /home/daniel.barrington/dev/bfs/ou/system; ant -f Build.xml -Ddbusername=root -Ddbpassword=Pa55w0rd -Dmysql.root.password=Pa55w0rd -Ddbhost=127.0.0.1 teardown'
